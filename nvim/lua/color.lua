b9de040f6cc358aba27bfa2d549569ffb9639fe3
mame77 <morinaga.kota@icloud.com>
2025-09-25 05:53:15 +0900

    fix: waybarのアイコン化

--- nvim/lua/color.lua
+++ nvim/lua/color.lua
@@ -1,9 +1,10 @@
 vim.pack.add({
 	'https://github.com/folke/tokyonight.nvim',
-    'https://github.com/shaunsingh/nord.nvim'
+    'https://github.com/shaunsingh/nord.nvim',
+    'https://github.com/catppuccin/nvim',
 })
 
-vim.cmd.colorscheme('nord')
+vim.cmd.colorscheme('catppuccin-mocha')
 
 vim.api.nvim_set_hl(0, "CursorLineNr", {
   fg = "#ffffff",     -- 白
