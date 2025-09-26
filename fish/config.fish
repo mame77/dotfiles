871b96ae012c3462605665871863731bb9aa68d4
mame77 <morinaga.kota@icloud.com>
2025-09-27 02:43:17 +0900

    feat: tmux

--- fish/config.fish
+++ fish/config.fish
@@ -19,9 +19,33 @@ function mkcd
     cd $argv[1]
 end
 
-# git commit -m "OO" → gc OO
-function gc
-    git commit -m $argv
+# git checkout OO → go OO
+function go
+    git checkout $argv
 end
 
+# git checkout -b OO → gb OO
+function gb
+    git checkout -b $argv
+end
+
+# git add O → ga O
+function ga
+    git add $argv
+end
+
+# git status → gs
+function gs
+    git status
+end
+
+# git commit -m "OO" → gc OO
+function gc
+    git commit -m "$argv"
+end
+
+# git push origin OO → gp OO
+function gp
+    git push origin $argv
+end
 
