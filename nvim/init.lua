65824478b510383d925bb286345f95a64a931b92
mame77 <morinaga.kota@icloud.com>
2025-11-02 11:34:06 +0900

    feat: gitsignsの追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
--- mouse
+-- display
 vim.opt.mouse = ""
 vim.opt.guicursor = ""
--- display
 vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.signcolumn = "yes"
@@ -16,11 +15,10 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
--- search
 vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.diagnostic.config({ virtual_text = true })
--- auto completion
+-- completion
 vim.opt.pumheight = 5
 vim.opt.completeopt = { "fuzzy", "menuone", "noselect" }
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -33,74 +31,50 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end
     end,
 })
+
 -- plugin
 vim.pack.add({
     { src = 'https://github.com/nvim-mini/mini.pick' },                 -- minipick
     { src = 'https://github.com/echasnovski/mini.pairs' },              -- minipairs
-    { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' }, -- blankline
+    { src = 'https://github.com/lewis6991/gitsigns.nvim' },             -- gitsigns
+    { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' }, -- ibl
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },     -- treesitter
     { src = 'https://github.com/neovim/nvim-lspconfig' },               -- lspconfig
-    -- notification
-    { src = 'https://github.com/MunifTanjim/nui.nvim'},                 -- nui
-    { src = 'https://github.com/rcarriga/nvim-notify'},                 -- notify
-    { src = 'https://github.com/folke/noice.nvim'},                     -- noice
-    { src = 'https://github.com/github/copilot.vim' },               -- tokyonight
-    -- colorscheme
+    { src = 'https://github.com/MunifTanjim/nui.nvim' },                -- nui
+    { src = 'https://github.com/rcarriga/nvim-notify' },                -- notify
+    { src = 'https://github.com/folke/noice.nvim' },                    -- noice
     { src = 'https://github.com/folke/tokyonight.nvim' },               -- tokyonight
 });
 require('mini.pick').setup()
 require('mini.pairs').setup()
--- blankline
-require("ibl").setup({
-    indent = { char = "│", },
-        scope = {
-        enabled = false,
-        show_start = true,
-        show_end = true,
-    } })
--- treesitter 
+require('gitsigns').setup()
+require("ibl").setup({ indent = { char = "│" }, scope = { enabled = false } })
 require('nvim-treesitter.configs').setup({
     ensure_installed = { 'lua', 'go', 'typescript', 'javascript', 'html', 'css' },
     auto_install = true,
     highlight = { enable = true },
 })
--- lsp
 vim.lsp.enable('gopls')
 vim.lsp.config('lua_ls', {
-    settings = {
-        Lua = {
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-        },
-    },
+    settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("", true) } } }
 })
--- notify
-require("notify").setup({
-  timeout = 1500,
-  stages = "static",
-  top_down = false,
-})
--- noice
-require("noice").setup({
-  presets = { command_palette = true },
-})
--- colorscheme
+require("notify").setup({ timeout = 1500, stages = "static", top_down = false })
+require("noice").setup({ presets = { command_palette = true } })
 vim.cmd.colorscheme('tokyonight-night')
-vim.api.nvim_set_hl(0, "CursorLineNr", {
-  fg = "#ffffff",
-  bold = true,
-})
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffffff", bold = true })
+
 -- keymap
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>j', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>k', vim.cmd.w)
-vim.keymap.set('n', '<leader>t', vim.cmd.terminal)
+vim.keymap.set('n', '<leader>t', vim.cmd.term)
 vim.keymap.set('n', '<leader>x', vim.cmd.bd)
-vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y')
-vim.keymap.set({'n', 'v', 'x'}, '<leader>p', '"+p')
--- minipick
+vim.keymap.set('n', '<C-j>', 'o<Esc>')
+vim.keymap.set({'v', 'x'}, '<leader>y', '"+y')
+
 vim.keymap.set('n', '<leader>f', ':Pick files<CR>');
 vim.keymap.set('n', '<leader>g', ':Pick grep_live<CR>');
-vim.keymap.set('n', '<leader>e', ':Pick buffers<CR>');
+vim.keymap.set('n', '<leader>r', ':Pick buffers<CR>');
+vim.keymap.set('n', '[p', '<cmd>Gitsigns prev_hunk<CR>')
+vim.keymap.set('n', '[n', '<cmd>Gitsigns next_hunk<CR>')
 
